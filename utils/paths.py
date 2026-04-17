d79eecab90553ea371d2cca7705ed0793c61b356
Ayush <sudhirsoni9889@gmail.com>
2026-04-17 12:25:51 +0530

    make a resolve_path function

---
+++ utils/paths.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+
+
+def resolve_path(base : str | Path,path : str | Path) -> Path:
+    p = Path(path)
+    if p.is_absolute():
+        return p.resolve()
+    return (Path(base) / p).resolve()
+
+
+def is_binary_file(path :str| Path) -> bool:
+    try:
+        with open(path, "rb") as f:
+           chunk =  f.read(8192)
+           return f"\x00" in chunk
+    except Exception:
+        return False
